f04a1fe932d0636233821e72d88a516832be943d
KseniaYatskevich <kseniayatskevich@gmail.com>
2019-10-06 19:52:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,47 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const space="**********";
+    
+    const words = expr.split(space);
+    const letters = words
+        .map(word => {
+            let i = 0;
+            const result = [];
+            while (i < word.length) {
+                result.push(word.slice(i, i + 10));
+                i = i + 10;
+            }
+            return result;
+        });
+    const symbols = letters
+        .map(arr => arr.map(letter => {
+            let i = 0;
+            const result = [];
+            while (i < letter.length) {
+                result.push(letter.slice(i, i + 2));
+                i = i + 2;
+            }
+            return result;
+        }));
+    const decoded = symbols
+        .map(word => word
+        .map(letter => {
+            const res = [];
+            for (let i = letter.length - 1; i >= 0; i--) {
+                if (letter[i] === '10') res.unshift('.');
+                if (letter[i] === '11') res.unshift('-');
+                if (letter[i] === '00') break;
+            }
+            return res.join('');
+        }))
+
+    const answer = decoded
+        .map(word => word
+        .map(letter => MORSE_TABLE[letter]))
+        .map(word => word.join(''))
+        .join(' ');
+    return answer;
 }
 
 module.exports = {
